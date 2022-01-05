075d797f48e80eff4e4fbe8434d2819c4848afd0
IlsaKhan <96765501+IlsaKhan@users.noreply.github.com>
2022-01-05 14:45:03 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,18 +3,17 @@ import signal
 import time
 
 class GracefulKiller:
-  kill_now = False
-  def __init__(self):
-    signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+    def exit_gracefully(self, *args):
+        self.kill_now = True
     
-   def exit_gracefully(self, *args):
-    self.kill_now = True
-    
-   if __name__ == '__main__':
+if __name__ == '__main__':
     killer = GracefulKiller()
     while not killer.kill_now:
-      time.sleep(1)
-      print("Hello world!")
+        time.sleep(1)
+        print("Hello world!")
 
       
