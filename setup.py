d07e055d2d0afc55bca685d7a0fd1aead2f06183
IlsaKhan <96765501+IlsaKhan@users.noreply.github.com>
2022-01-05 16:03:14 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 
 setup (
-    name             = "hello",
-    version          = "3.5",
+    name             = "Python",
+    version          = "1.0",
     description      = "Example application to be deployed.",
     packages         = find_packages(),
     install_requires = ["gunicorn"],
