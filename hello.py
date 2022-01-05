ae6fd3b0da03f041da0890eeee2da3521c55a8c4
IlsaKhan <96765501+IlsaKhan@users.noreply.github.com>
2022-01-05 14:22:05 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,20 @@
 # This program prints Hello, world!
+import signal
+import time
 
-print("Hello world!")
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+    
+   def exit_gracefully(self, *args):
+    self.kill_now = True
+    
+   if __name__ == '__main__':
+    killer = GracefulKiller()
+    while not killer.kill_now:
+      time.sleep(1)
+      print("Hello world!")
+
+      
